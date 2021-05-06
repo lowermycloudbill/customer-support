843a385d5fe2d8070851a4e3339270b435c3d1a2
Ronaldo Campos <ronaldo.campos@scalac.io>
2021-05-06 15:25:47 +0100

    try this

--- azure-cloud-shell/azure-cloud-shell-cloudadmin.sh
+++ azure-cloud-shell/azure-cloud-shell-cloudadmin.sh
@@ -44,14 +44,48 @@ fi
 echo '== Assigning roles to the app'
 sleep 8s # a new service principal is not usable for role assignment instantly, neither after couple of seconds.. :)
 
-# echo '. Assigning Contributor role to CAST.AI app'
-# az role assignment create --assignee "$CASTAI_APP_ID" --role 'Contributor' -o none
-
-echo '. Assigning Contributor role to CAST.AI shared images app'
+echo '. Assigning Reservation Purchaser role to app'
 az role assignment create --assignee "$APP_ID" --role 'Reservation Purchaser' -o none
 
-### Print results
+NAME="CloudAdminService"
 
+ROLE_DEFINITION="{
+   \"Name\": \"$NAME\",
+   \"Description\": \"Required permissions for Cloudadmin Services\",
+   \"IsCustom\": true,
+   \"AssignableScopes\": [
+       \"/subscriptions/$SELECTED_SUBSCRIPTION_ID\"
+   ],
+   \"Actions\": [
+        \"*/read\",
+        \"Microsoft.Compute/virtualMachines/start/action\",
+        \"Microsoft.Compute/virtualMachines/restart/action\",
+        \"Microsoft.Compute/virtualMachines/powerOff/action\",
+        \"Microsoft.Compute/disks/delete\",
+        \"Microsoft.Compute/snapshots/delete\",
+        \"Microsoft.Sql/servers/databases/resume/action\",
+        \"Microsoft.Sql/servers/databases/pause/action\",
+        \"Microsoft.Network/loadBalancers/delete\",
+        \"Microsoft.Network/publicIPAddresses/delete\"
+   ],
+   \"NotActions\": []
+}"
+
+echo '. Checking if custom role exists'
+listing=$(az role definition list --name "$NAME")
+
+if [ "$listing" ]; then
+  echo '. Creating custom role for CloudAdmin'
+  az role definition create --role-definition "$ROLE_DEFINITION"
+else
+  echo '. Updating custom role for CloudAdmin'
+  az role definition update --role-definition "$ROLE_DEFINITION"
+fi
+
+echo '. Assigning custom role to app'
+az role assignment create --assignee "$APP_ID" --role "$NAME" -o none
+
+### Print results
 echo "--------------------------------------------------------------------------------"
 echo "Save and use the following json to onboard credentials into CloudAdmin"
 cat << EOF
